a8305fde56cb0a246d1ef27fc749e729cd103ab3
Nathaniel <naune15@punahou.edu>
2014-11-04 19:54:51 -1000

    Activity 2
    Added responses for pets, teachers, and various other words.

--- Magpie.java
+++ Magpie.java
@@ -12,71 +12,97 @@
  */
 public class Magpie
 {
- /**
-  * Get a default greeting  
-  * @return a greeting
-  */
- public String getGreeting()
- {
-  return "Hello, let's talk.";
- }
- 
- /**
-  * Gives a response to a user statement
-  * 
-  * @param statement
-  *            the user statement
-  * @return a response based on the rules given
-  */
- public String getResponse(String statement)
- {
-  String response = "";
-  if (statement.indexOf("no") >= 0)
+  /**
+   * Get a default greeting  
+   * @return a greeting
+   */
+  public String getGreeting()
   {
-   response = "Why so negative?";
+    return "Hello, let's talk.";
   }
-  else if (statement.indexOf("mother") >= 0
-    || statement.indexOf("father") >= 0
-    || statement.indexOf("sister") >= 0
-    || statement.indexOf("brother") >= 0)
-  {
-   response = "Tell me more about your family.";
-  }
-  else
-  {
-   response = getRandomResponse();
-  }
-  return response;
- }
-
- /**
-  * Pick a default response to use if nothing else fits.
-  * @return a non-committal string
-  */
- private String getRandomResponse()
- {
-  final int NUMBER_OF_RESPONSES = 4;
-  double r = Math.random();
-  int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-  String response = "";
   
-  if (whichResponse == 0)
+  /**
+   * Gives a response to a user statement
+   * 
+   * @param statement
+   *            the user statement
+   * @return a response based on the rules given
+   */
+  public String getResponse(String statement)
   {
-   response = "Interesting, tell me more.";
+    String response = "";
+    if (statement.trim().length() <= 0)
+    {
+      response = ("Say something I'm giving up on you~~");
+    }
+    else if (statement.indexOf("the") >= 0)
+    {
+      response = "Please, continue talking. This is very interesting.";
+    }
+    else if (statement.indexOf("it") >= 0)
+    {
+      response = "Agh! Don't say that word!";
+    }
+    else if (statement.indexOf("no") >= 0)
+    {
+      response = "Why so negative?";
+    }
+    else if (statement.indexOf("name") >= 0)
+    {
+      response = "I'm no good at remembering names.";
+    }
+    else if (statement.indexOf("mother") >= 0
+               || statement.indexOf("father") >= 0
+               || statement.indexOf("sister") >= 0
+               || statement.indexOf("brother") >= 0)
+    {
+      response = "Tell me more about your family.";
+    }
+    else if (statement.indexOf("cat") >= 0
+               || statement.indexOf("dog") >=0)
+    {
+      response = "Tell me more about your pets.";
+    }
+    else if (statement.indexOf("Kiang") >= 0
+               || statement.indexOf("Landgraaf") >= 0)
+    {
+      response = "You know, that's a pretty nifty teacher you got there.";
+    } 
+    else
+    {
+      response = getRandomResponse();
+    }
+    return response;
   }
-  else if (whichResponse == 1)
+  
+  /**
+   * Pick a default response to use if nothing else fits.
+   * @return a non-committal string
+   */
+  private String getRandomResponse()
   {
-   response = "Hmmm.";
+    final int NUMBER_OF_RESPONSES = 4;
+    double r = Math.random();
+    int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+    String response = "";
+    
+    if (whichResponse == 0)
+    {
+      response = "Interesting, tell me more.";
+    }
+    else if (whichResponse == 1)
+    {
+      response = "Hmmm.";
+    }
+    else if (whichResponse == 2)
+    {
+      response = "Do you really think so?";
+    }
+    else if (whichResponse == 3)
+    {
+      response = "You don't say.";
+    }
+    
+    return response;
   }
-  else if (whichResponse == 2)
-  {
-   response = "Do you really think so?";
-  }
-  else if (whichResponse == 3)
-  {
-   response = "You don't say.";
-  }
-
-  return response;
- }
 }
