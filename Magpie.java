23260b49e022255ba8b737ac6b46e34b9e16d769
Nathaniel <naune15@punahou.edu>
2014-11-06 12:33:26 -1000

    Activity 3
    Added advanced keyword searches

--- Magpie.java
+++ Magpie.java
@@ -35,36 +35,36 @@ public class Magpie
     {
       response = ("Say something I'm giving up on you~~");
     }
-    else if (statement.indexOf("the") >= 0)
+    else if (findKeyword(statement, "the", 0) >= 0)
     {
       response = "Please, continue talking. This is very interesting.";
     }
-    else if (statement.indexOf("it") >= 0)
+    else if (findKeyword(statement, "it", 0) >= 0)
     {
       response = "Agh! Don't say that word!";
     }
-    else if (statement.indexOf("no") >= 0)
+    else if (findKeyword(statement, "no", 0) >= 0)
     {
       response = "Why so negative?";
     }
-    else if (statement.indexOf("name") >= 0)
+    else if (findKeyword(statement, "name", 0) >= 0)
     {
       response = "I'm no good at remembering names.";
     }
-    else if (statement.indexOf("mother") >= 0
-               || statement.indexOf("father") >= 0
-               || statement.indexOf("sister") >= 0
-               || statement.indexOf("brother") >= 0)
+    else if (findKeyword(statement, "mother", 0) >= 0
+               || findKeyword(statement, "father", 0) >= 0
+               || findKeyword(statement, "sister", 0) >= 0
+               || findKeyword(statement, "brother", 0) >= 0)
     {
       response = "Tell me more about your family.";
     }
-    else if (statement.indexOf("cat") >= 0
-               || statement.indexOf("dog") >=0)
+    else if (findKeyword(statement, "cat", 0) >= 0
+               || (findKeyword(statement, "dog", 0) >=0))
     {
       response = "Tell me more about your pets.";
     }
-    else if (statement.indexOf("Kiang") >= 0
-               || statement.indexOf("Landgraaf") >= 0)
+    else if (findKeyword(statement, "Kiang", 0) >= 0
+               || (findKeyword(statement, "Landgraaf", 0) >= 0))
     {
       response = "You know, that's a pretty nifty teacher you got there.";
     } 
@@ -79,6 +79,77 @@ public class Magpie
    * Pick a default response to use if nothing else fits.
    * @return a non-committal string
    */
+  
+   private int findKeyword(String statement, String goal,
+   int startPos)
+ {
+  String phrase = statement.trim();
+  // The only change to incorporate the startPos is in
+  // the line below
+  int psn = phrase.toLowerCase().indexOf(
+    goal.toLowerCase(), startPos);
+
+  // Refinement--make sure the goal isn't part of a
+  // word
+  while (psn >= 0)
+  {
+   // Find the string of length 1 before and after
+   // the word
+   String before = " ", after = " ";
+   if (psn > 0)
+   {
+    before = phrase.substring(psn - 1, psn)
+      .toLowerCase();
+   }
+   if (psn + goal.length() < phrase.length())
+   {
+    after = phrase.substring(
+      psn + goal.length(),
+      psn + goal.length() + 1)
+      .toLowerCase();
+   }
+
+   // If before and after aren't letters, we've
+   // found the word
+   if (((before.compareTo("a") < 0) || (before
+     .compareTo("z") > 0)) // before is not a
+           // letter
+     && ((after.compareTo("a") < 0) || (after
+       .compareTo("z") > 0)))
+   {
+    return psn;
+   }
+
+   // The last position didn't work, so let's find
+   // the next, if there is one.
+   psn = phrase.indexOf(goal.toLowerCase(),
+     psn + 1);
+
+  }
+
+  return -1;
+ }
+
+ /**
+  * Search for one word in phrase. The search is not case
+  * sensitive. This method will check that the given goal
+  * is not a substring of a longer string (so, for
+  * example, "I know" does not contain "no"). The search
+  * begins at the beginning of the string.
+  * 
+  * @param statement
+  *            the string to search
+  * @param goal
+  *            the string to search for
+  * @return the index of the first occurrence of goal in
+  *         statement or -1 if it's not found
+  */
+ private int findKeyword(String statement, String goal)
+ {
+  return findKeyword(statement, goal, 0);
+ }
+  
+  
   private String getRandomResponse()
   {
     final int NUMBER_OF_RESPONSES = 4;
