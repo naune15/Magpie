34ac16b5577455582acdc64170f93095d9639bb9
Nathaniel <naune15@punahou.edu>
2014-11-11 08:44:17 -1000

    Activity 4
    Added ways for Magpie to respond to phrases.

--- Magpie.java
+++ Magpie.java
@@ -68,9 +68,40 @@ public class Magpie
     {
       response = "You know, that's a pretty nifty teacher you got there.";
     } 
+    else if (findKeyword(statement, "I want to", 0) >= 0)
+    {
+      response = transformIWantToStatement(statement);
+    }
+    
+    else
+  {
+
+   // Look for a two word (you <something> me)
+   // pattern
+   int psn = findKeyword(statement, "you", 0);
+
+   if (psn >= 0
+     && findKeyword(statement, "me", psn) >= 0)
+   {
+    response = transformYouMeStatement(statement);
+   }
+   else
+   {
+    //  Part of student solution
+    // Look for a two word (I <something> you)
+    // pattern
+    psn = findKeyword(statement, "i", 0);
+
+    if (psn >= 0
+      && findKeyword(statement, "you", psn) >= 0)
+    {
+     response = transformIYouStatement(statement);
+    }
     else
     {
-      response = getRandomResponse();
+     response = getRandomResponse();
+    }
+   }
     }
     return response;
   }
@@ -80,100 +111,140 @@ public class Magpie
    * @return a non-committal string
    */
   
-   private int findKeyword(String statement, String goal,
-   int startPos)
- {
-  String phrase = statement.trim();
-  // The only change to incorporate the startPos is in
-  // the line below
-  int psn = phrase.toLowerCase().indexOf(
-    goal.toLowerCase(), startPos);
-
-  // Refinement--make sure the goal isn't part of a
-  // word
-  while (psn >= 0)
+  private String transformYouMeStatement(String statement)
   {
-   // Find the string of length 1 before and after
-   // the word
-   String before = " ", after = " ";
-   if (psn > 0)
-   {
-    before = phrase.substring(psn - 1, psn)
-      .toLowerCase();
-   }
-   if (psn + goal.length() < phrase.length())
-   {
-    after = phrase.substring(
-      psn + goal.length(),
-      psn + goal.length() + 1)
-      .toLowerCase();
-   }
-
-   // If before and after aren't letters, we've
-   // found the word
-   if (((before.compareTo("a") < 0) || (before
-     .compareTo("z") > 0)) // before is not a
-           // letter
-     && ((after.compareTo("a") < 0) || (after
-       .compareTo("z") > 0)))
-   {
-    return psn;
-   }
-
-   // The last position didn't work, so let's find
-   // the next, if there is one.
-   psn = phrase.indexOf(goal.toLowerCase(),
-     psn + 1);
-
-  }
-
-  return -1;
- }
-
- /**
-  * Search for one word in phrase. The search is not case
-  * sensitive. This method will check that the given goal
-  * is not a substring of a longer string (so, for
-  * example, "I know" does not contain "no"). The search
-  * begins at the beginning of the string.
-  * 
-  * @param statement
-  *            the string to search
-  * @param goal
-  *            the string to search for
-  * @return the index of the first occurrence of goal in
-  *         statement or -1 if it's not found
-  */
- private int findKeyword(String statement, String goal)
- {
-  return findKeyword(statement, goal, 0);
- }
-  
-  
-  private String getRandomResponse()
-  {
-    final int NUMBER_OF_RESPONSES = 4;
-    double r = Math.random();
-    int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
-    String response = "";
-    
-    if (whichResponse == 0)
+    //  Remove the final period, if there is one
+    statement = statement.trim();
+    String lastChar = statement.substring(statement
+                                            .length() - 1);
+    if (lastChar.equals("."))
     {
-      response = "Interesting, tell me more.";
-    }
-    else if (whichResponse == 1)
-    {
-      response = "Hmmm.";
-    }
-    else if (whichResponse == 2)
-    {
-      response = "Do you really think so?";
-    }
-    else if (whichResponse == 3)
-    {
-      response = "You don't say.";
+      statement = statement.substring(0, statement
+                                        .length() - 1);
     }
     
-    return response;
+    int psnOfYou = findKeyword (statement, "you", 0);
+    int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+    
+    String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
+    return "What makes you think that I " + restOfStatement + " you?";
   }
+  private String transformIYouStatement(String statement)
+  {
+    statement = statement.trim();
+    String lastChar = statement.substring(statement.length() - 1);
+    if (lastChar.equals("."))
+    {
+      statement = statement.substring(0, statement.length() - 1);
+    }
+  int psnOfI = findKeyword (statement, "I", 0);
+  int psnOfYou = findKeyword (statement, "you", psnOfI + 1);
+  String restOfStatement = statement.substring(psnOfI + 1, psnOfYou).trim();
+  return "Why do you " + restOfStatement + " me?";
 }
+   
+    private String transformIWantToStatement(String statement)
+    {
+      //  Remove the final period, if there is one
+      statement = statement.trim();
+      String lastChar = statement.substring(statement
+                                              .length() - 1);
+      if (lastChar.equals("."))
+      {
+        statement = statement.substring(0, statement
+                                          .length() - 1);
+      }
+      int psn = findKeyword (statement, "I want to", 0);
+      String restOfStatement = statement.substring(psn + 9).trim();
+      return "What would it mean to " + restOfStatement + "?";
+    }
+    
+    private int findKeyword(String statement, String goal,
+                            int startPos)
+    {
+      String phrase = statement.trim();
+      // The only change to incorporate the startPos is in
+      // the line below
+      int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
+      
+      // Refinement--make sure the goal isn't part of a
+      // word
+      while (psn >= 0)
+      {
+        // Find the string of length 1 before and after
+        // the word
+        String before = " ", after = " ";
+        if (psn > 0)
+        {
+          before = phrase.substring(psn - 1, psn)
+            .toLowerCase();
+        }
+        if (psn + goal.length() < phrase.length())
+        {
+          after = phrase.substring(psn + goal.length(), psn + goal.length() + 1).toLowerCase();
+        }
+        
+        // If before and after aren't letters, we've
+        // found the word
+        if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before is not a letter
+              && ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
+        {
+          return psn;
+        }
+        
+        // The last position didn't work, so let's find
+        // the next, if there is one.
+        psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
+        
+      }
+      
+      return -1;
+    }
+    
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no"). The search
+     * begins at the beginning of the string.
+     * 
+     * @param statement
+     *            the string to search
+     * @param goal
+     *            the string to search for
+     * @return the index of the first occurrence of goal in
+     *         statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal)
+    {
+      return findKeyword(statement, goal, 0);
+    }
+    
+    
+    private String getRandomResponse()
+    {
+      final int NUMBER_OF_RESPONSES = 4;
+      double r = Math.random();
+      int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+      String response = "";
+      
+      if (whichResponse == 0)
+      {
+        response = "Interesting, tell me more.";
+      }
+      else if (whichResponse == 1)
+      {
+        response = "Hmmm.";
+      }
+      else if (whichResponse == 2)
+      {
+        response = "Do you really think so?";
+      }
+      else if (whichResponse == 3)
+      {
+        response = "You don't say.";
+      }
+      
+      return response;
+    }
+    }
